bf8574a4a6896f1d869e9035e06ba9d694e74832
ashmna <mnatsakanyan.ashot@gmail.com>
2015-10-21 19:54:55 +0400

    Added chrome_extension

---
+++ resources/chrome_extension/content.js
@@ -0,0 +1,11 @@
+//function click(selector) {
+//    var el = document.querySelector(selector);
+//    if(el && el.fireEvent) {
+//        el.fireEvent('onclick');
+//    }
+//}
+//
+//click("#gsri_ok0");
+
+
+
