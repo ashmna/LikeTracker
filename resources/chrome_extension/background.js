e003ac3260bd2c46202d5056104769d742ea7947
ashmna <mnatsakanyan.ashot@gmail.com>
2015-10-23 19:03:30 +0400

    Improvements Chrome extension

--- resources/chrome_extension/background.js
+++ resources/chrome_extension/background.js
@@ -1,29 +1,54 @@
-var taskList = {};
+(function(){
+    var taskList = {};
 
-chrome.extension.onMessage.addListener(function (request) {
-    if (!request) return;
-    if (!request.task) return;
-    var task = request.task;
-
-    switch (request.type) {
-        case "lt.task.add":
-            taskList[task.id] = task;
-            break;
-        case "lt.task.remove":
-            if(taskList[task.id])
-                delete taskList[task.id];
-            break;
-        case "lt.task.get":
-            if(task.url) {
-                for(var id in taskList) {
-                    if(taskList[id].url == task.url) {
-                        task = taskList[id];
-                        break;
-                    }
-                }
-            }
-            break;
+    function addTask(task) {
+        if(task) {
+            taskList[task.url] = task;
+            return true;
+        } else {
+            return false;
+        }
     }
-    console.log(taskList);
-});
 
+    function removeTask(task) {
+        if(task && taskList[task.url]) {
+            delete taskList[task.url];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function getTask(url) {
+        if(taskList[url])
+            return taskList[url];
+        else
+            return null;
+    }
+
+
+
+    chrome.extension.onMessage.addListener(function(request, sender, callback){
+        if (!request) return;
+        if (!request.cmd) return;
+
+        var task = null;
+        if(request.task)
+            task = request.task;
+
+        switch (request.cmd) {
+            case "lt.task.add":
+                callback(addTask(task));
+                break;
+            case "lt.task.remove":
+                callback(removeTask(task));
+                break;
+            case "lt.task.get":
+                callback(getTask(request.url));
+                break;
+        }
+
+    });
+
+
+})();
--- resources/chrome_extension/content.js
+++ resources/chrome_extension/content.js
@@ -1,13 +1,77 @@
-var dispatchMouseEvent = function(target, var_args) {
-    var e = document.createEvent("MouseEvents");
-    e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1));
-    target.dispatchEvent(e);
-};
+(function(){
+    var dispatchMouseEvent = function(target, var_args) {
+        var e = document.createEvent("MouseEvents");
+        e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1));
+        target.dispatchEvent(e);
+    };
+    var click = function (selector) {
+        var element = document.querySelector(selector);
+        dispatchMouseEvent(element, 'click', true, true);
+    };
+    var convertUrl = function (url) {
+        var res = '';
+        var pos = url.indexOf('vk.com/');
 
-function click(selector) {
-    var element = document.querySelector(selector);
-    dispatchMouseEvent(element, 'click', true, true);
-}
+        if(pos != -1) {
+            url = url.substr(pos+7);
+            pos = url.indexOf('?');
+            if(pos != -1) {
+                url = url.substr(pos+1);
+                var pair, obj  = {}, vars = url.split('&');
+                for (var i = 0; i < vars.length; i++) {
+                    pair = vars[i].split('=');
+                    obj[pair[0]] = pair[1];
+                }
+                if(obj['z']) {
+                    res = url = obj['z'];
+                } else if(obj['w']) {
+                    res = url = obj['w'];
+                }
+                pos = url.indexOf('%2F');
+                if(pos != -1) {
+                    res = url.substr(0, pos);
+                } else {
+                    pos = url.indexOf('/');
+                    if(pos != -1) {
+                        res = url.substr(0, pos);
+                    }
+                }
+            } else {
+                res = url;
+            }
+        }
+        return res;
+    };
+
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            main();
+        }
+    };
+
+
+    function main() {
+        var url = convertUrl(window.location.href);
+        if(url) {
+            var request = {
+                cmd : 'lt.task.get',
+                url : url
+            };
+            chrome.extension.sendMessage(request, function(task) {
+                if(task) {
+                    console.log(task);
+                }
+            });
+        }
+
+
+    }
+
+
+
+})();
+
+
 
 //dispatchMouseEvent(element, 'mouseover', true, true);
 //dispatchMouseEvent(element, 'mousedown', true, true);
@@ -19,26 +83,6 @@ function click(selector) {
 //
 //});
 
-function main() {
-    console.log(chrome);
-    //var task = getTask();
-    //if(task) {
-    //    switch (task.type) {
-    //        case 'ki' :
-    //
-    //            break;
-    //    }
-    //}
-
-
-
-    chrome.extension.sendMessage('запрос backMsg', function(backMessage){
-        console.log('2. Обратно принято из фона:', backMessage);
-    });
-    // http://habrahabr.ru/post/174745/
-
-
-}
 
 
 
@@ -83,12 +127,6 @@ function main() {
 
 
 
-
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        main();
-    }
-};
 
 
 
